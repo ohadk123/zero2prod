9f4302db42d6b08e131086c756d330b85cf3b4c4
Ohad Katz <ohad43@gmail.com>
2025-08-08 12:52:40 +0300

    fixed formatting

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,14 @@
 use std::{io::Error, net::TcpListener};
 
-use actix_web::{dev::Server, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{App, HttpResponse, HttpServer, Responder, dev::Server, web};
 async fn health_check() -> impl Responder {
     HttpResponse::Ok()
 }
 
-pub fn run(listener: TcpListener) -> Result<Server, Error>{
-    let server = HttpServer::new(|| {
-        App::new()
-            .route("/health_check", web::get().to(health_check))
-    })
-    .listen(listener)?
-    .run();
+pub fn run(listener: TcpListener) -> Result<Server, Error> {
+    let server = HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
+        .listen(listener)?
+        .run();
 
     Ok(server)
 }
