ece73c75792a161093630514c114ecd7ef290e2b
Ohad Katz <ohad43@gmail.com>
2025-08-09 19:01:30 +0300

    Chapter 3.7 "Working With HTML Forms"

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,8 @@
 use std::net::TcpListener;
-
 use zero2prod::run;
 
 #[tokio::main]
-async fn main() {
+async fn main() -> std::io::Result<()> {
     let listener = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind to address");
-    let server = run(listener).expect("Failed to run on address");
-    tokio::spawn(server);
+    run(listener)?.await
 }
