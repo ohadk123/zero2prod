0491f95e1323ef3027569aad16397ed0b55a8d9b
Ohad Katz <ohad43@gmail.com>
2025-08-20 11:28:14 +0300

    Chapter 5.3 "A Dockerfile For Our Application"

--- src/configuration.rs
+++ src/configuration.rs
@@ -1,10 +1,16 @@
-use config::{self, Config, ConfigError, File};
+use config::{self, Config, ConfigError};
 use secrecy::{ExposeSecret, SecretString};
 
 #[derive(serde::Deserialize)]
 pub struct Settings {
     pub database: DataBaseSettings,
-    pub application_port: u16,
+    pub application: ApplicationSettings,
+}
+
+#[derive(serde::Deserialize)]
+pub struct ApplicationSettings {
+    pub port: u16,
+    pub host: String,
 }
 
 #[derive(serde::Deserialize)]
@@ -17,10 +23,50 @@ pub struct DataBaseSettings {
 }
 
 pub fn get_configuration() -> Result<Settings, ConfigError> {
+    let configuration_directory = std::env::current_dir()
+        .expect("Failed to determine the current directory")
+        .join("configuration");
+
+    let environment: Environment = std::env::var("APP_ENVIRONMENT")
+        .unwrap_or_else(|_| "local".into())
+        .try_into()
+        .expect("Failed to parse APP_ENVIRONMENT");
+
     Config::builder()
-        .add_source(File::with_name("configuration"))
+        .add_source(config::File::from(configuration_directory.join("base")).required(true))
+        .add_source(
+            config::File::from(configuration_directory.join(environment.as_str())).required(true),
+        )
         .build()?
         .try_deserialize()
+}
+
+pub enum Environment {
+    Local,
+    Production,
+}
+
+impl Environment {
+    pub fn as_str(&self) -> &'static str {
+        match self {
+            Self::Local => "local",
+            Self::Production => "production",
+        }
+    }
+}
+
+impl TryFrom<String> for Environment {
+    type Error = String;
+
+    fn try_from(value: String) -> Result<Self, Self::Error> {
+        match value.to_lowercase().as_str() {
+            "local" => Ok(Self::Local),
+            "production" => Ok(Self::Production),
+            other => Err(format!(
+                "{other} is not supported environement. Use either `local` or `production`."
+            )),
+        }
+    }
 }
 
 impl DataBaseSettings {
