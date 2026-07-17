1e22dd8ed3488441dcb7e68de11d9f6afc4f44b0
Ohad Katz <ohad87@proton.me>
2026-07-17 16:12:19 +0300

    3.4 Out First Ingeration Test

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,6 @@
-use std::io::Error;
-
-use actix_web::{App, HttpResponse, HttpServer, Responder, web};
-
-async fn health_check() -> impl Responder {
-    HttpResponse::Ok()
-}
+use zero2prod::run;
 
 #[tokio::main]
-async fn main() -> Result<(), Error> {
-    HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
-        .bind("127.0.0.1:8000")?
-        .run()
-        .await
+async fn main() -> Result<(), std::io::Error> {
+    run().await
 }
