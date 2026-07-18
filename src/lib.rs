4f818810b247d2c8b4a2ad769f1d206c09851b80
Ohad Katz <ohad87@proton.me>
2026-07-18 19:33:26 +0300

    3.9 Persisting A New Subscriber

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,3 @@
 pub mod configurations;
 pub mod routes;
 pub mod startup;
-
-use actix_web::{App, HttpServer, dev::Server, web};
-use routes::{health_check, subscribe};
-use std::net::TcpListener;
-
-pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| {
-        App::new()
-            .route("/health_check", web::get().to(health_check))
-            .route("/subscriptions", web::post().to(subscribe))
-    })
-    .listen(listener)?
-    .run();
-
-    Ok(server)
-}
